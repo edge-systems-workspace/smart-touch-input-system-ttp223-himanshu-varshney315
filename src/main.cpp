6cf9b9a42ae253f074117d4a9723d16dbae8b402
himanshu-varshney315 <himanshu.gupta_cs.aiml24@gla.acin>
2026-02-19 08:48:46 +0530

    remove TODO comments and clean up setup function in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,25 +10,12 @@
  * and displays structured output via Serial Monitor.
  */
 
- // TODO 1:
- // Define touch sensor digital pin (Use pin 2)
-
- // TODO 2:
- // Create variable to store touch state
 int touchPin=7;
 int ledPin=13;
 int buzzerPin=13;
 
 void setup() {
 
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
-
-    // TODO 4:
-    // Configure touch pin as INPUT
-
-    // TODO 5:
-    // Print system initialization message
 
     pinMode(touchPin,INPUT);
     pinMode(ledPin,OUTPUT);
@@ -36,17 +23,6 @@ void setup() {
 }
 void loop() {
 
-    // TODO 6:
-    // Read digital value from touch sensor
-
-    // TODO 7:
-    // If touch detected (HIGH)
-    //     Print "Touch Detected"
-    // Else
-    //     Print "No Touch"
-
-    // TODO 8:
-    // Add small delay (200–500ms)
     int state=digitalRead(touchPin);
     if(state==1)
     {
